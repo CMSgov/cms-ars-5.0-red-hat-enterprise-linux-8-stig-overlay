620129bf6809edf6d7ca7a66a309f53864338017
Sumaa Sayed <ssayed1@umbc.edu>
2022-07-14 09:52:54 -0400

    added control 230483 test
    Signed-off-by: Sumaa Sayed <ssayed1@umbc.edu>

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -586,5 +586,38 @@ include_controls 'redhat-enterprise-linux-8-stig-baseline' do
 
           $ sudo systemctl restart sshd.service"
   end
+
+  control 'SV-230483' do
+    desc 'title', "RHEL 8 must take action when allocated audit record storage volume
+    reaches 80 percent of the repository maximum audit record storage capacity."
+  end
+
+  control 'SV-230483' do
+    desc 'check', "Verify RHEL 8 takes action when allocated audit record storage volume
+    reaches 80 percent of the repository maximum audit record storage capacity with
+    the following commands:
+    
+        $ sudo grep -w space_left /etc/audit/auditd.conf
+    
+        space_left = 20%
+    
+        If the value of the \"space_left\" keyword is not set to \"20%\" or if the
+    line is commented out, ask the System Administrator to indicate how the system
+    is providing real-time alerts to the SA and ISSO.
+    
+        If there is no evidence that real-time alerts are configured on the system,
+    this is a finding."
+  end
+
+  control 'SV-230483' do
+    desc 'fix', "Configure the operating system to initiate an action to notify the SA and
+    ISSO (at a minimum) when allocated audit record storage volume reaches 80
+    percent of the repository maximum audit record storage capacity by
+    adding/modifying the following line in the /etc/audit/auditd.conf file.
+    
+        space_left = 20%
+    
+        Note: Option names and values in the auditd.conf file are case insensitive."
+  end
     #if system_categorization == 'MODERATE': // moderate overlays go here //
 end
