17195e9e1f4cae09b71e068fdce31bad4ff9a849
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2022-10-02 21:50:23 -0400

    updates based on shifts between 800-53 rev4 and 5
    - NIST tags updated due to changes between NIST SP 800-53 rev 4 and rev 5 (https://csrc.nist.gov/csrc/media/Publications/sp/800-53/rev-5/final/documents/sp800-53r4-to-r5-comparison-workbook.xlsx)
    - IA-2(6) incorporates withdrawn controls IA-2(7) and IA-2(11)
    - SC-45(1) incorporates withdrawn control AU-8(1)
    - AC-2(3) maintains requirement to disable accounts, removed from rev 4's IA-5(1)"(e)"
    
    As a result SC-230373 is *not* N/A (removed caveat)

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -12,6 +12,32 @@ include_controls 'redhat-enterprise-linux-8-stig-baseline' do
     end
   end
 
+  
+  ## NIST tags updated due to changes between NIST SP 800-53 rev 4 and rev 5 (https://csrc.nist.gov/csrc/media/Publications/sp/800-53/rev-5/final/documents/sp800-53r4-to-r5-comparison-workbook.xlsx)
+
+  ## IA-2(6) incorporates withdrawn controls IA-2(7) and IA-2(11)
+
+  control 'SV-230273' do
+    tag nist: ["IA-2 (6)"]
+  end
+  
+  control 'SV-230274' do
+    tag nist: ["IA-2 (6)"]
+  end
+
+  ## SC-45(1) incorporates withdrawn control AU-8(1)
+  
+  control 'SV-230484' do
+    tag nist: ["SC-45 (1)"]
+  end
+  
+  ## AC-2(3) maintains requirement to disable accounts, removed from rev 4's IA-5(1)"(e)"
+  
+  control "SV-230373" do
+    tag nist: ["AC-2 (3)"]
+  end
+
+
   control 'SV-230229' do
     desc 'fix', "Configure RHEL 8, for PKI-based authentication, to validate certificates by
     constructing a certification path (which includes status information) to an
@@ -21,11 +47,6 @@ include_controls 'redhat-enterprise-linux-8-stig-baseline' do
     bundle and copy the #{input('org_name')[:acronym]}_PKE_CA_chain.pem into the following file:
     
         /etc/sssd/pki/sssd_auth_ca_db.pem"
-  end
-
-  control 'SV-230373' do
-    impact 0.0
-    desc "caveat", "Not applicable for this CMS ARS 5.0 overlay, since this requirement is not included in CMS ARS 5.0."
   end
 
   control 'SV-230484' do
